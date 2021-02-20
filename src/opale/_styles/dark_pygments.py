543fcd7063c6e8b7321829ce03a53dbbe5737264
Alessandro Candido <candido.ale@gmail.com>
2021-02-20 18:22:25 +0100

    Define pygments style for dark mode

--- src/opale/_styles/dark_pygments.py
+++ src/opale/_styles/dark_pygments.py
@@ -24,68 +24,71 @@ class Dark(Style):
 
     styles = {
         # No corresponding class for the following:
-        Text: "#7b869d",  # class:  ''
-        Whitespace: "underline #7b869d",  # class: 'w'
-        Error: "#7b869d",  # class: 'err'
-        Other: "#7b869d",  # class 'x'
-        Comment: "italic #7b869d",  # class: 'c'
-        Comment.Preproc: "noitalic",  # class: 'cp'
-        Keyword: "bold #7b869d",  # class: 'k'
-        Keyword.Constant: "bold #7b869d",  # class: 'kc'
-        Keyword.Declaration: "bold #7b869d",  # class: 'kd'
-        Keyword.Namespace: "bold #7b869d",  # class: 'kn'
-        Keyword.Pseudo: "bold #7b869d",  # class: 'kp'
-        Keyword.Reserved: "bold #7b869d",  # class: 'kr'
-        Keyword.Type: "bold #7b869d",  # class: 'kt'
-        Operator: "#7b869d",  # class: 'o'
-        Operator.Word: "bold #7b869d",  # class: 'ow' - like keywords
-        Punctuation: "#7b869d",  # class: 'p'
-        # because special names such as Name.Class, Name.Function, etc.
-        # are not recognized as such later in the parsing, we choose them
-        # to look the same as ordinary variables.
-        Name: "#7b869d",  # class: 'n'
-        Name.Attribute: "#7b869d",  # class: 'na' - to be revised
-        Name.Builtin: "#7b869d",  # class: 'nb'
-        Name.Builtin.Pseudo: "#7b869d",  # class: 'bp'
-        Name.Class: "#7b869d",  # class: 'nc' - to be revised
-        Name.Constant: "#7b869d",  # class: 'no' - to be revised
-        Name.Decorator: "#7b869d",  # class: 'nd' - to be revised
-        Name.Entity: "#7b869d",  # class: 'ni'
-        Name.Exception: "bold #7b869d",  # class: 'ne'
-        Name.Function: "#7b869d",  # class: 'nf'
-        Name.Property: "#7b869d",  # class: 'py'
-        Name.Label: "#7b869d",  # class: 'nl'
-        Name.Namespace: "#7b869d",  # class: 'nn' - to be revised
-        Name.Other: "#7b869d",  # class: 'nx'
-        Name.Tag: "bold #7b869d",  # class: 'nt' - like a keyword
-        Name.Variable: "#7b869d",  # class: 'nv' - to be revised
-        Name.Variable.Class: "#7b869d",  # class: 'vc' - to be revised
-        Name.Variable.Global: "#7b869d",  # class: 'vg' - to be revised
-        Name.Variable.Instance: "#7b869d",  # class: 'vi' - to be revised
-        Number: "#7b869d",  # class: 'm'
-        Literal: "#7b869d",  # class: 'l'
-        Literal.Date: "#7b869d",  # class: 'ld'
-        String: "#7b869d",  # class: 's'
-        String.Backtick: "#7b869d",  # class: 'sb'
-        String.Char: "#7b869d",  # class: 'sc'
-        String.Doc: "italic #7b869d",  # class: 'sd' - like a comment
-        String.Double: "#7b869d",  # class: 's2'
-        String.Escape: "#7b869d",  # class: 'se'
-        String.Heredoc: "#7b869d",  # class: 'sh'
-        String.Interpol: "#7b869d",  # class: 'si'
-        String.Other: "#7b869d",  # class: 'sx'
-        String.Regex: "#7b869d",  # class: 'sr'
-        String.Single: "#7b869d",  # class: 's1'
-        String.Symbol: "#7b869d",  # class: 'ss'
-        Generic: "#7b869d",  # class: 'g'
-        Generic.Deleted: "#7b869d",  # class: 'gd'
-        Generic.Emph: "italic #7b869d",  # class: 'ge'
-        Generic.Error: "#7b869d",  # class: 'gr'
-        Generic.Heading: "bold #7b869d",  # class: 'gh'
-        Generic.Inserted: "#7b869d",  # class: 'gi'
-        Generic.Output: "#7b869d",  # class: 'go'
-        Generic.Prompt: "#7b869d",  # class: 'gp'
-        Generic.Strong: "bold #7b869d",  # class: 'gs'
-        Generic.Subheading: "bold #7b869d",  # class: 'gu'
-        Generic.Traceback: "bold #7b869d",  # class: 'gt'
+        Text: "#dddee1",  # class:  ''
+        Whitespace: "underline #181d25",  # class: 'w'
+        Error: "#bf616a",  # class: 'err'
+        Other: "",  # class 'x'
+        Comment: "italic #424754",  # class: 'c'
+        Comment.Hashbang: "#7b869d",  # class: 'ch'
+        Comment.Multiline: "",  # class:
+        Comment.Preproc: "noitalic #7884a0",  # class: 'cp'
+        Comment.PreprocFile: "#9d97c3",  # class: 'cpf'
+        Comment.Single: "",  # class: 'cl'
+        Comment.Special: "",  # class:
+        Keyword: "#5e81ac",  # class: 'k'
+        Keyword.Constant: "bold #5e81ac",  # class: 'kc'
+        # Keyword.Declaration: "bold #7b869d",  # class: 'kd'
+        # Keyword.Namespace: "bold #7b869d",  # class: 'kn'
+        # Keyword.Pseudo: "bold #7b869d",  # class: 'kp'
+        # Keyword.Reserved: "bold #7b869d",  # class: 'kr'
+        # Keyword.Type: "bold #7b869d",  # class: 'kt'
+        Literal: "",  # class: 'l'
+        Literal.Date: "",  # class: 'ld'
+        Name: "",  # class: 'n'
+        Name.Attribute: "",  # class: 'na' - to be revised
+        Name.Builtin: "italic #81a1c1",  # class: 'nb'
+        Name.Builtin.Pseudo: "#5e81ac",  # class: 'bp'
+        Name.Class: "#88c0d0",  # class: 'nc' - to be revised
+        Name.Constant: "",  # class: 'no' - to be revised
+        Name.Decorator: "",  # class: 'nd' - to be revised
+        Name.Entity: "",  # class: 'ni'
+        Name.Exception: "#bf616a",  # class: 'ne'
+        Name.Function: "#88c0d0",  # class: 'nf'
+        Name.Property: "",  # class: 'py'
+        Name.Label: "",  # class: 'nl'
+        Name.Namespace: "",  # class: 'nn' - to be revised
+        Name.Other: "",  # class: 'nx'
+        Name.Tag: "bold",  # class: 'nt' - like a keyword
+        Name.Variable: "",  # class: 'nv' - to be revised
+        Name.Variable.Class: "",  # class: 'vc' - to be revised
+        Name.Variable.Global: "",  # class: 'vg' - to be revised
+        Name.Variable.Instance: "",  # class: 'vi' - to be revised
+        Number: "#b48ead",  # class: 'm'
+        Number.Float: "#b48ead",  # class: 'mf'
+        Operator: "#eaddc8",  # class: 'o'
+        Operator.Word: "bold",  # class: 'ow' - like keywords
+        Punctuation: "",  # class: 'p'
+        String: "#a3be8c",  # class: 's'
+        # String.Backtick: "#7b869d",  # class: 'sb'
+        # String.Char: "#7b869d",  # class: 'sc'
+        String.Doc: "italic #88a37b",  # class: 'sd' - like a comment
+        # String.Double: "#7b869d",  # class: 's2'
+        String.Escape: "#ebcb8b",  # class: 'se'
+        # String.Heredoc: "#7b869d",  # class: 'sh'
+        String.Interpol: "bold #97b67c",  # class: 'si'
+        # String.Other: "#7b869d",  # class: 'sx'
+        String.Regex: "#d08770",  # class: 'sr'
+        # String.Single: "#7b869d",  # class: 's1'
+        # String.Symbol: "#7b869d",  # class: 'ss'
+        Generic: "#d08770",  # class: 'g'
+        Generic.Deleted: "",  # class: 'gd'
+        Generic.Emph: "italic",  # class: 'ge'
+        Generic.Error: "",  # class: 'gr'
+        Generic.Heading: "bold",  # class: 'gh'
+        Generic.Inserted: "",  # class: 'gi'
+        Generic.Output: "",  # class: 'go'
+        Generic.Prompt: "",  # class: 'gp'
+        Generic.Strong: "bold",  # class: 'gs'
+        Generic.Subheading: "bold",  # class: 'gu'
+        Generic.Traceback: "bold",  # class: 'gt'
     }
